124a52e0d20ca5469ac13e48509a5f7a8ce6761a
Damian Montero <photoca@hotmail.com>
2017-03-31 23:10:54 -0400

    initial commit

---
+++ downloadwebsite.sh
@@ -0,0 +1,10 @@
+wget \
+--recursive \
+--no-clobber \
+--page-requisites \
+--html-extension \
+--convert-links \
+--restrict-file-names=windows \
+--domains html5up.net \
+--no-parent \
+    https://html5up.net/uploads/demos/ethereal/
